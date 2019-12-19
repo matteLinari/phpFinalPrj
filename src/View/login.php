877a5246d35614d69c4d9cd8c8e9bac5a1ca1804
Ubuntu <matteo.linari@edu.itspiemonte.it>
2019-12-19 06:56:08 -0800

    added regitration

--- src/View/login.php
+++ src/View/login.php
@@ -1,25 +1,27 @@
 <?php $this->layout('layout', ['title' => 'Login']) ?>
 
 <div class="container-fluid" style="margin-top: 50;">
-<h1 class ="text-center">Login</h1></br></br></br>
-<form role="form" method="post" action="/login" method="POST">
 
-    <div class="form-group row">
-        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-            <input type="email" name="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+    <h1 class="text-center">Login</h1></br></br></br>
+    <p><?= $this->e($msg) ?></p>
+    <form role="form" method="post" action="/login" method="POST">
+
+        <div class="form-group row">
+            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" name="email" class="form-control" id="inputEmail" name="email" placeholder="Email">
+            </div>
         </div>
-    </div>
-    <div class="form-group row">
-        <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-        <div class="col-sm-10">
-            <input type="password" name="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+        <div class="form-group row">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" name="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
+            </div>
         </div>
-    </div>
-    <div class="form-group row">
-        <div class="offset-sm-2 col-sm-10">
-            <input type="submit" value="Sign in" name="submit" class="btn btn-primary"/>
+        <div class="form-group row">
+            <div class="offset-sm-2 col-sm-10">
+                <input type="submit" value="Sign in" name="submit" class="btn btn-primary" />
+            </div>
         </div>
-    </div>
-</form>
+    </form>
 </div>
